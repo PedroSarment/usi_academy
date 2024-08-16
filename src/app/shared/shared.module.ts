48fbe9294eba0eeb5f4fe4667b50a4fefae3a071
Pedro Sarmento <phsp@cin.ufpe.br>
2024-08-16 11:51:45 -0300

    feat: created content-card component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { UserCardComponent } from './components/user-card/user-card.component';
-
+import { TrainingCardComponent } from './components/training-card/training-card.component';
+import { ContentCardComponent } from './components/content-card/content-card.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent, 
-    UserCardComponent],
+    UserCardComponent,
+    TrainingCardComponent,
+    ContentCardComponent
+  ],
   exports: [
     HeaderComponent, 
-    UserCardComponent],
+    UserCardComponent,
+    TrainingCardComponent,
+    ContentCardComponent
+  ],
   imports: [
     CommonModule,
     IonicModule,
